149abf69efcb9c317e272e999d2c80d115969528
raul <raul.alexander.floress@gmail.com>
2022-09-05 23:47:50 -0700

    html

--- contracts/Todo.sol
+++ contracts/Todo.sol
@@ -3,4 +3,23 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract Todo{
     uint public taskCount = 0;
+
+    struct Task{
+        uint id;
+        string content;
+        bool completed;
+
+    }
+    mapping(uint =>Task) public tasks;
+
+    constructor() public {
+        createTask("Hello There");
+    }
+
+    function createTask(string memory _content) public{
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+
+    }
+
 }
---
+++ src/app.js
@@ -0,0 +1,63 @@
+
+App = {
+    contracts:{},
+    load:async()=>{
+        await App.loadWeb3()
+        await App.loadAccount()
+        await App.loadContract()
+        console.log("app loading...")
+    },
+      // https://medium.com/metamask/https-medium-com-metamask-breaking-change-injecting-web3-7722797916a8
+  loadWeb3: async () => {
+    if (typeof web3 !== 'undefined') {
+      App.web3Provider = web3.currentProvider
+      web3 = new Web3(web3.currentProvider)
+    } else {
+      window.alert("Please connect to Metamask.")
+    }
+    // Modern dapp browsers...
+    if (window.ethereum) {
+      window.web3 = new Web3(ethereum)
+      try {
+        // Request account access if needed
+        await ethereum.enable()
+        // Acccounts now exposed
+        web3.eth.sendTransaction({/* ... */})
+      } catch (error) {
+        // User denied account access...
+      }
+    }
+    // Legacy dapp browsers...
+    else if (window.web3) {
+      App.web3Provider = web3.currentProvider
+      window.web3 = new Web3(web3.currentProvider)
+      // Acccounts always exposed
+      web3.eth.sendTransaction({/* ... */})
+    }
+    // Non-dapp browsers...
+    else {
+      console.log('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  },
+
+  loadAccount: async () => {
+    App.account = await web3.eth.accounts[0]
+
+    console.log(App.accounts)
+  },
+
+  loadContract: async ()=>{
+     // Create a JavaScript version of the smart contract
+     const todo = await $.getJSON('Todo.json')
+     App.contracts.Todo = TruffleContract(todo)
+     App.contracts.Todo.setProvider(App.web3Provider)
+ 
+  }
+
+}
+
+$(()=>{
+    $(window).load(() => {
+        App.load()
+    })
+})
